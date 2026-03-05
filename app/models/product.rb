390aedc22379e8a4a5ccde1aa29f2488f234cadf
EduardBogoslovtcev2 <ebogoslovtcev@rrc.ca>
2026-03-05 11:57:46 -0600

    added validation for the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
-class Product < ApplicationRecord
+class Product < ActiveRecord::Base
+  
+  # The columns of the object are:
+  # Product title (string), description (text), price (decimal), stock_quantity (integer)
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :stock_quantity, presence: true
 end
